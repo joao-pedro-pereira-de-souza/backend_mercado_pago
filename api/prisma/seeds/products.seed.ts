8dbdccc43c7b62d72640ff31bfb8d6a6ab748b52
Joao Pedro <joao.pedro@dotcoding.com.br>
2024-08-30 01:03:44 -0300

    feature: add route get object type products and ajust seed produtcs

--- api/prisma/seeds/products.seed.ts
+++ api/prisma/seeds/products.seed.ts
@@ -1,12 +1,15 @@
 import products from './contents/products.json';
 import { PrismaTransactionalClient } from '../connection';
+
+import { typesProducts } from '../../src/contents/products';
+
 export class ProductsSeed {
     #prisma;
     constructor(prisma: PrismaTransactionalClient) {
         this.#prisma = prisma;
     }
 
-    async #create() {
+    async #createProductBee() {
         const productBee = products.find((item) => item.type === 'product_bee');
 
         const isProductExists = await this.#prisma.product.findFirst({
@@ -22,7 +25,7 @@ export class ProductsSeed {
                     type: productBee.type,
                     options: {
                         createMany: {
-                            data: productBee.options,
+                            data: productBee.options as any[],
                         },
                     },
                 },
@@ -30,7 +33,42 @@ export class ProductsSeed {
         }
     }
 
+    async #createProductHoneyPot() {
+        const productHoneyPot = products.find((item) => item.type === 'product_honey_pot');
+        const { value_ml, minimum_ml, maximum_ml, ...dataProduct } =
+            productHoneyPot as any;
+
+        await this.#prisma.product.create({
+            data: {
+                ...dataProduct,
+                payments_ml: {
+                    create: {
+                        value_ml,
+                        minimum_ml,
+                        maximum_ml,
+                    },
+                },
+            },
+        });
+    }
+
+    async #createProductHive() {
+        const productHive = products.find(
+            (item) => item.type === typesProducts.hive
+        ) as any;
+
+
+        await this.#prisma.product.create({
+            data: {
+                ...productHive
+
+            },
+        });
+    }
+
     async inicialize() {
-        await this.#create();
+        await this.#createProductBee();
+        await this.#createProductHoneyPot();
+        await this.#createProductHive();
     }
 }
--- api/src/contents/products.ts
+++ api/src/contents/products.ts
@@ -1,3 +1,5 @@
-export const typesProducts =  {
-    bee: 'product_bee'
+export const typesProducts = {
+    bee: 'product_bee',
+    honey_pot: 'product_honey_pot',
+    hive: 'product_hive',
 };
